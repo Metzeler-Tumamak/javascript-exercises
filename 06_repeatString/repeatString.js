525abbcd4b641447c72637d88bac49775cbacf79
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2025-09-18 22:00:35 +0800

    Complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (text, count) {
+  if (count < 0) return "ERROR";
+  let repeatedText = "";
 
+  for (let i = 0; i < count; i++) {
+    repeatedText += text;
+  }
+
+  return repeatedText.trim();
 };
 
 // Do not edit below this line

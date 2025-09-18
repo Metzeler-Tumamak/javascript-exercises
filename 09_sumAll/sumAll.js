f42c9ace96b404597e99f2667d214523c6a3f8de
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2025-09-18 22:27:21 +0800

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (
+    !Array.from(arguments).every((elem) => Number.isInteger(elem) && elem >= 0)
+  )
+    return "ERROR";
+  const min = a < b ? a : b;
+  const max = a > b ? a : b;
+  let sum = 0;
 
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line

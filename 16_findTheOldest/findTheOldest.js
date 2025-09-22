8bcce575fa953b4c28b5d9e6ad026e975d70ecd1
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2025-09-22 22:29:44 +0800

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const newArr = people.map((person) =>
+    person.yearOfDeath
+      ? { ...person }
+      : { ...person, yearOfDeath: new Date().getFullYear() }
+  );
 
+  newArr.sort(
+    (a, b) => a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth)
+  );
+
+  return newArr.at(-1);
 };
 
 // Do not edit below this line
